3836ffa0e052c60edd9a28e440b9c9a0e31a6a90
Jorge Turiel Fernández <jorge@jorge-lx>
2025-08-27 20:58:38 +0200

    Capitulo 1 duplicado

---
+++ Capitulo2/Ghost1/code/gameviewmain.pas
@@ -0,0 +1,106 @@
+{ Main view, where most of the application logic takes place.
+
+  Feel free to use this code as a starting point for your own projects.
+  This template code is in public domain, unlike most other CGE code which
+  is covered by BSD or LGPL (see https://castle-engine.io/license). }
+unit GameViewMain;
+
+interface
+
+uses Classes,
+  CastleVectors, CastleComponentSerialize,
+  CastleUIControls, CastleControls, CastleKeysMouse, CastleCameras;
+
+type
+  { Main view, where most of the application logic takes place. }
+  TViewMain = class(TCastleView)
+  published
+    { Components designed using CGE editor.
+      These fields will be automatically initialized at Start. }
+    LabelFps: TCastleLabel;
+    Navigation2D1: TCastle2DNavigation;
+  public
+    constructor Create(AOwner: TComponent); override;
+    procedure Start; override;
+    procedure Update(const SecondsPassed: single; var HandleInput: boolean); override;
+    function Press(const Event: TInputPressRelease): boolean; override;
+  end;
+
+var
+  ViewMain: TViewMain;
+
+implementation
+
+uses SysUtils;
+
+  { TViewMain ----------------------------------------------------------------- }
+
+constructor TViewMain.Create(AOwner: TComponent);
+begin
+  inherited;
+  DesignUrl := 'castle-data:/gameviewmain.castle-user-interface';
+end;
+
+procedure TViewMain.Start;
+begin
+  inherited;
+end;
+
+procedure TViewMain.Update(const SecondsPassed: single; var HandleInput: boolean);
+var
+  PosicionCamara: TVector2;
+begin
+  inherited;
+  { This virtual method is executed every frame (many times per second). }
+  Assert(LabelFps <> nil,
+    'If you remove LabelFps from the design, remember to remove also the assignment "LabelFps.Caption := ..." from code');
+  LabelFps.Caption := 'FPS: ' + Container.Fps.ToString;
+  if Container.Pressed[keyArrowRight] then
+  begin
+    PosicionCamara := Navigation2D1.Camera.TranslationXY;
+    PosicionCamara := PosicionCamara + Vector2(150 * SecondsPassed, 0);
+    if PosicionCamara.X > 1615 then
+    begin
+      PosicionCamara := Vector2(1615, 0);
+    end;
+    Navigation2D1.Camera.TranslationXY := PosicionCamara;
+  end
+  else if Container.Pressed[keyArrowLeft] then
+  begin
+    PosicionCamara := Navigation2D1.Camera.TranslationXY;
+    PosicionCamara := PosicionCamara - Vector2(150 * SecondsPassed, 0);
+    if PosicionCamara.X < -1615 then
+    begin
+      PosicionCamara := Vector2(-1615, 0);
+    end;
+    Navigation2D1.Camera.TranslationXY := PosicionCamara;
+  end;
+
+end;
+
+function TViewMain.Press(const Event: TInputPressRelease): boolean;
+begin
+  Result := inherited;
+  if Result then Exit; // allow the ancestor to handle keys
+
+  { This virtual method is executed when user presses
+    a key, a mouse button, or touches a touch-screen.
+
+    Note that each UI control has also events like OnPress and OnClick.
+    These events can be used to handle the "press", if it should do something
+    specific when used in that UI control.
+    The TViewMain.Press method should be used to handle keys
+    not handled in children controls.
+  }
+
+  // Use this to handle keys:
+  {
+  if Event.IsKey(keyXxx) then
+  begin
+    // DoSomething;
+    Exit(true); // key was handled
+  end;
+  }
+end;
+
+end.
